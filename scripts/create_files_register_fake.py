ef3d9c98d005e034853c5b0adf430b171added7c
AyrtonCossuol <ayrton.cossuol@live.com>
2022-09-11 16:15:04 -0300

    :tada: Initial commit

---
+++ scripts/create_files_register_fake.py
@@ -0,0 +1,30 @@
+from typing import Any
+from faker import Faker
+from datetime import datetime
+import random as r
+import time
+import os
+
+def generate_register_fake(file, fake, qt_register_fake: int):
+    """
+    Funcao que ira gerar os registros de forma aleatoria e inserir dentro do arquivo csv
+    :file: arquivo que sera inseridos os registros fakes criados.
+    :fake: instancia do pacote Fake
+    :qt_register_fake: quantidade aleatoria de registros a serem criados e inseridos no arquivo csv
+
+    return
+        Arquivo com a quantidade de registros inseridos de acordo com o valor de qt_register_fake.
+     """
+
+    
+
+def create_file_fake() -> None:
+    """
+    Funcao que ira gerar uma quantidade N de arquivos com registros fakes.
+    """
+   
+    return None
+
+if __name__ == '__main__':
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\ayrto\OneDrive\Área de Trabalho\students\project-student-account.json"
+    create_file_fake()
